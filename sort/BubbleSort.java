978ba8351dc03ed7d913bdf0d7e51c6cee3194fb
Thea-X <18241107761@163.com>
2015-11-29 16:49:51 +0800

    冒泡排序java实现

---
+++ sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package algorithm.sort;
+/**
+ *冒泡算法的学习 by studying ahalei
+ *@author Thea
+ *冒泡排序的基本思想是：每次比较两个相邻的元素，如果他们的顺序错误就把他们交换过来。
+ *冒泡排序的时间复杂度是O(N2)。
+ */
+public class BubbleSort {
+	public static void main(String[] args){
+		int[] array = {99,35,35,76,12};
+		
+		for(int i = 0; i < array.length; i ++){
+			for(int j = 0; j < array.length - 1 - i; j ++){
+				if(array[j] < array[j + 1]){
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < array.length; i ++){
+			System.out.println(array[i]);
+		}
+	}
+}
